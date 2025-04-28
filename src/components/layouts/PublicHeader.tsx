4dd537c40ee6c34018ee32935ecaecceb0d6fe59
ryoma-abe <liaozhenanbu387@gmail.com>
2025-04-28 19:08:21 +0900

    依存関係に@radix-ui/react-tooltipを追加し、package.jsonとpackage-lock.jsonを更新しました。

---
+++ src/components/layouts/PublicHeader.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
+export default function PublicHeader() {
+  return (
+    <div>
+      <header className="bg-blue-200 border-b">
+        <div className="container mx-auto p-4 flex items-center justify-between">
+          
+        </div>
+      </header>
+    </div>
+  );
+}
