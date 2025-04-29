6975199d699483834389ecc7219694aba4890b84
ryoma-abe <liaozhenanbu387@gmail.com>
2025-04-30 08:53:52 +0900

    PostPageコンポーネントにトップ画像の表示機能を追加し、Cardコンポーネントのスタイルを改善しました。画像が存在する場合にのみ表示されるように条件を設定しました。

--- src/app/(public)/posts/[id]/page.tsx
+++ src/app/(public)/posts/[id]/page.tsx
@@ -1,7 +1,6 @@
 import { getPost } from "@/lib/post";
 import { notFound } from "next/navigation";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { PostCardProps } from "@/types/post";
 import Image from "next/image";
 type Params = {
   params: Promise<{ id: string }>;
@@ -15,7 +14,19 @@ export default async function PostPage({ params }: Params) {
   }
   return (
     <div className="container mx-auto px-4 py-8">
-      <Card>
+      <Card className="max-w-3xl mx-auto">
+        {post.topImage && (
+          <div className="w-full relative h-64 lg:h-96">
+            <Image
+              src={post.topImage}
+              alt={post.title}
+              fill
+              sizes="100vw"
+              className="rounded-t-md object-cover"
+              priority
+            />
+          </div>
+        )}
         <CardHeader>
           <CardTitle></CardTitle>
         </CardHeader>
