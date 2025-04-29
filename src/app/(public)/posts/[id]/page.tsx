8aa82b838add0401c05a73cd38b756e73d4e9a56
ryoma-abe <liaozhenanbu387@gmail.com>
2025-04-30 08:51:47 +0900

    PostPageコンポーネントにCardコンポーネントを追加し、投稿の表示レイアウトを改善しました。また、CardHeaderとCardContentを使用して構造を整理しました。

--- src/app/(public)/posts/[id]/page.tsx
+++ src/app/(public)/posts/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { getPost } from "@/lib/post";
 import { notFound } from "next/navigation";
-
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { PostCardProps } from "@/types/post";
+import Image from "next/image";
 type Params = {
   params: Promise<{ id: string }>;
 };
@@ -12,8 +14,13 @@ export default async function PostPage({ params }: Params) {
     notFound();
   }
   return (
-  <div>
-    
-  </div>
-);
+    <div className="container mx-auto px-4 py-8">
+      <Card>
+        <CardHeader>
+          <CardTitle></CardTitle>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
+    </div>
+  );
 }
