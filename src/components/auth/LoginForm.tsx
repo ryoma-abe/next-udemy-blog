708e10b90e5879210bfce4e8fe6e118e541ce1da
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-02 07:48:12 +0900

    LoginFormコンポーネントの修正：コールバックURLの取得を削除し、エラーメッセージ表示機能を追加しました。また、ボタンに「ログイン」というラベルを追加しました。

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -2,15 +2,12 @@
 
 import { useActionState } from "react";
 import { authenticate } from "@/lib/actions/authenticate";
-import { useSearchParams } from "next/navigation";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 
 export default function LoginForm() {
-  const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get("callbackUrl") || "/dashboard";
   const [errorMessage, formAction, isPending] = useActionState(
     authenticate,
     undefined
@@ -32,7 +29,16 @@ export default function LoginForm() {
               <Label htmlFor="password">パスワード</Label>
               <Input id="password" type="password" name="password" required />
             </div>
-            <Button></Button>
+            <Button type="submit" className="w-full">
+              ログイン
+            </Button>
+            <div className="flex h-8 items-end space-x-1">
+              {errorMessage && (
+                <div className="text-red-500">
+                  <p className="text-sm text-red-500">{errorMessage}</p>
+                </div>
+              )}
+            </div>
           </form>
         </CardContent>
       </Card>
