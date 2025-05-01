9728e3eaf65ab271affa769517853f21d7774b1f
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-01 21:33:25 +0900

    auth.tsにユーザー取得関数を追加し、メールアドレスとパスワードの照合処理を実装しました。これにより、認証プロセスが強化され、ユーザーの認証が可能になります。

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,14 @@ import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
+import { prisma } from "./lib/prisma";
+import bcryptjs from "bcryptjs";
+
+async function getUser(email: string) {
+  return await prisma.user.findUnique({
+    where: { email: email },
+  });
+}
 
 export const { auth, signIn, signOut, handlers } = NextAuth({
   ...authConfig,
@@ -11,6 +19,17 @@ export const { auth, signIn, signOut, handlers } = NextAuth({
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(8) })
           .safeParse(credentials);
+
+        if (!parsedCredentials.success) return null;
+
+        const { email, password } = parsedCredentials.data;
+        const user = await getUser(email);
+        if (!user) return null;
+
+        const passwordsMatch = await bcryptjs.compare(password, user.password);
+        if (!passwordsMatch) return null;
+
+        return user;
       },
     }),
   ],
